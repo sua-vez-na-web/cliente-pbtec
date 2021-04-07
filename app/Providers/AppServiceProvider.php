e28aa9e4e8e772938177ee1c095cf1a28ebd76df
matthausnawan <matthausnawan@gmail.com>
2021-04-07 15:59:19 -0300

    remove force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env("APP_ENV") === "production") {
-            URL::forceScheme('https');
-        }
+        // if (env("APP_ENV") === "production") {
+        //     URL::forceScheme('https');
+        // }
     }
 
     /**
